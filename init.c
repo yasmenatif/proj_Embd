c9640998e4e15b6893ed94b28e61758fa3fc3df8
thanaashabana <85203133+thanaashabana@users.noreply.github.com>
2021-06-06 05:02:26 +0200

    Add files via upload

---
+++ init.c
@@ -0,0 +1,21 @@
+#include "stdint.h"
+#include "C:/keil/tm4c123gh6pm.h"
+void SystemInit(){}
+void init_PortA_PortB (){
+SYSCTL_RCGCGPIO_R |= 0x01; //Enable PORTA clock
+while((SYSCTL_PRGPIO_R & 0x02)==0){};
+SYSCTL_RCGCGPIO_R |= 0x02; //Enable PORT B clock
+while((SYSCTL_PRGPIO_R & 0x02)==0){};
+GPIO_PORTA_DIR_R |=0xE0; //PORTA pins 5-6-7 controls RS,E and R/W
+GPIO_PORTA_DEN_R |=0xE0;
+GPIO_PORTA_AMSEL_R =0;
+GPIO_PORTA_AFSEL_R =0;
+GPIO_PORTA_PCTL_R = 0;
+GPIO_PORTB_DIR_R |=0xFF; //PORTB 0-7
+GPIO_PORTB_DEN_R |=0xFF; //PORTB 0-7corresponds to D0-D7
+GPIO_PORTB_AMSEL_R =0;
+GPIO_PORTB_AFSEL_R =0;
+GPIO_PORTB_PCTL_R = 0;
+}
+int main(){
+}
