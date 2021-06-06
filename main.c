f97600b94ee0d90e962d5a876ff5b2bb982996fc
yasminfayed <85242751+yasminfayed@users.noreply.github.com>
2021-06-06 03:55:59 +0200

    Update main.c
    2

--- main.c
+++ main.c
@@ -5,15 +5,6 @@ int distance;
 int main(void){
 	
 	
-	SYSCTL_RCGCGPIO_R |=0x20;
-	while ((SYSCTL_PRGPIO_R&0x20)==0){};
-	GPIO_PORTF_LOCK_R = 0x4C4F434B;
-  GPIO_PORTF_CR_R |= 0x04;               
-  GPIO_PORTF_AMSEL_R = 0x00;        
-  GPIO_PORTF_PCTL_R = 0x00000000;  
-  GPIO_PORTF_DIR_R |= 0x04;            
-  GPIO_PORTF_AFSEL_R &= ~0x04;                     
-  GPIO_PORTF_DEN_R |= 0x04; 	
 	
 	distance = 101;
 	if (distance >= 100) {
