6179369043be008d2fc39c3bd76e233fe0e24248
thanaashabana <85203133+thanaashabana@users.noreply.github.com>
2021-06-14 00:11:31 +0200

    update of parsing

--- parsing.c
+++ parsing.c
@@ -1,39 +1,54 @@
 #include "stdint.h"
 #include "C:/keil/tm4c123gh6pm.h"
-#define MAXSIZE    100		/* GPS at most, sends 80 or so chars per message string.  So set maximum to 100 */
-#define COM_PORT   1		/* Serial device connected to COM 1 */
-#define SPEED      4800		/* baud rate = 4800 */
-#define CR         0x0d
-#define COMMA	  0x2C
+
 #include "stdio.h"
 #include "stdbool.h"
 #include "string.h"
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
-//#include "conio.h"
 #include "math.h"
 //#include "ibmcom3.h"
 void SystemInit(){}
 
- 
+ #define COM_PORT   3		/* Serial device connected to COM 3*/
+#define SPACE	  0x20
+#define COMMA	  0x2C
+#define MAXSIZE    100		/* GPS at most, sends 80 or so chars per message string.  So set maximum to 100 */
+
+#define CR         0x0d
+#define LF         0x0a
+#define pi 3.14159265358979323846
+
+	
+unsigned char  charRead;	     		
+unsigned char	 ReadString[MAXSIZE]; 		/* Buffer collects chars read from GPS */
+unsigned char  Stringtemp[MAXSIZE];
+  
+unsigned char  Stringlatitude[11];
+unsigned char  StringlatitudeCardinal[3];
+unsigned char  Stringlongitude[12];
+unsigned char  StringlongitudeCardinal[3];
+
+unsigned char  *pChar;
+unsigned char  dummyChar;
+  
+
+unsigned  char lastCommaPosition;
+double latitude;
+int	  latitudeDegrees;
+double	latitudeMinutes;
+
+double longitude;
+int		longitudeDegrees;
+double	longitudeMinutes;
+unsigned int   j, k;				
+unsigned int	 i;		     		//chars read per GPS message string 
+unsigned int	 LinesReadNO;        		// Number of GPS stringlines read
+double longitude_;
+double latitude_;
 int main(void){
 UARTInit();	
-
-
- 
-
-  
- unsigned int   j, k;				/* dummy variable */
-  unsigned int	 i;		     		/* Number of chars read per GPS message string */
-unsigned int ReadString;
-	unsigned char pChar;
-		double  Stringlongitude;
-		unsigned int	longitudeDegrees;
-			unsigned int longitudeMinutes; 
-	unsigned int  lastCommaPosition;
-	 unsigned intlongitude_ ;
-	 double longitude;
    //Rest of parsing function
 // Geting longitude 
 	      pChar = ReadString;
@@ -56,6 +71,6 @@ unsigned int ReadString;
 	      printf("/n");
 	  } // else not a GPGGA sentence 
 
-return (longitude_);
+return(longitude_);
       } 
 		 
